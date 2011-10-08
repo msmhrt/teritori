5a00490f11414ece2a2e9d5cf51f38642159e596
Masami HIRATA <msmhrt@gmail.com>
2011-10-08 17:27:47 +0900

    Added JSLint declarations

--- teritori.js
+++ teritori.js
@@ -22,6 +22,9 @@ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 THE SOFTWARE.
 */
 
+// JSLint declarations
+/*global $:false, alert:false, console:false, document:false, window:false */
+
 (function () {
     'use strict';
 
